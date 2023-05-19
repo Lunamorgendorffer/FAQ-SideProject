2b8f5118cb870a07ff807e1d31f2ed2f47d97d1d
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-19 17:07:48 +0200

    commentaires

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-const questions = document.querySelectorAll('.question');
-questions.forEach(function(question){
-    // console.log(question);
-    const btn =question.querySelector('.btn');
-    // console.log(btn);
-    btn.addEventListener('click',function(){
-        questions.forEach(function(item){
-            if(item!== question){
-                item.classList.remove('show-text');
-            }
-        });
+const questions = document.querySelectorAll(".question"); // Sélectionner tous les éléments avec la classe 'question' dans le document
 
-        question.classList.toggle('show-text');
+questions.forEach(function (question) { // Pour chaque élément 'question'
+  const btn = question.querySelector(".btn"); // Sélectionner le bouton à l'intérieur de l'élément 'question'
+  // console.log(btn);
+
+  btn.addEventListener("click", function () { // Ajouter un écouteur d'événements de clic au bouton
+    // console.log(question);
+
+    questions.forEach(function (item) { // Pour chaque élément 'question'
+      if (item !== question) { // Si l'élément actuel n'est pas celui sur lequel le bouton a été cliqué
+        item.classList.remove("show-text"); // Retirer la classe 'show-text' de l'élément pour masquer le texte
+      }
     });
+
+    question.classList.toggle("show-text"); // Ajouter ou retirer la classe 'show-text' de l'élément sur lequel le bouton a été cliqué pour afficher ou masquer le texte
+  });
 });
 
 
-// const btns = document.querySelectorAll('.btn');
+/* METHODE DOM*/
+// const btns = document.querySelectorAll('.btn'); // Sélectionner tous les éléments avec la classe 'btn' dans le document
 
-// btns.forEach(function(btn){
-//     btn.addEventListener('click', function(e){
-//         const question= e.currentTarget.parentElement.parentElement;
-//         question.classList.toggle('show-text');
+// btns.forEach(function(btn){ // Pour chaque élément avec la classe 'btn'
+//     btn.addEventListener('click', function(e){ // Ajouter un écouteur d'événements de clic au bouton
+//         const question= e.currentTarget.parentElement.parentElement; // Sélectionner l'élément parent du parent du bouton (l'élément 'question')
+//         question.classList.toggle('show-text'); // Ajouter ou retirer la classe 'show-text' de l'élément 'question' pour afficher ou masquer le texte
 
 //     });
-
-
-// })
+// });
