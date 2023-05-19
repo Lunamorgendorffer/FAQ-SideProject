99043ade5e380f763d009d100ca3c9bacc14e41e
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-19 16:50:37 +0200

    update

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const btns = document.querySelectorAll('.btn');
-
-btns.forEach(function(btn){
-    btn.addEventListener('click', function(e){
-        const question= e.currentTarget.parentElement.parentElement;
-        question.classList.toggle('show-text');
-
-    });
+const questions = document.querySelectorAll('.question');
+questions.forEach(function(question){
+    // console.log(question);
+    const btn =question.querySelector('.btn');
+    console.log(btn);
+});
 
 
-})
+// const btns = document.querySelectorAll('.btn');
+
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+//         const question= e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+
+//     });
+
+
+// })
