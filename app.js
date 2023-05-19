855250e61699f1887b0efbdc65efeb47eb9a7e07
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-19 16:36:16 +0200

    update

---
+++ app.js
@@ -0,0 +1,9 @@
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        console.log(e.currentTarget);
+    });
+
+
+})
